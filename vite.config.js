d84dbed93c535d22a0359bb03f54666786eb82e8
Shasika <32632851+Shasika@users.noreply.github.com>
2026-02-08 13:27:53 +0530

    Add laravel-vite-plugin and entrypoints

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [vue()],
-  root: '.',
+  plugins: [
+    laravel({
+      input: ['resources/js/main.js', 'resources/js/styles.css'],
+      refresh: true
+    }),
+    vue()
+  ],
   build: {
     outDir: 'public/build',
     emptyOutDir: true
