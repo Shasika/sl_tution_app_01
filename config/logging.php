efa731ac6aad91908a89ae3fa31ad09b0ebab979
Shasika <32632851+Shasika@users.noreply.github.com>
2026-02-08 13:11:48 +0530

    Add Laravel bootstrap and config files

---
+++ config/logging.php
@@ -0,0 +1,42 @@
+<?php
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
+
+return [
+    'default' => env('LOG_CHANNEL', 'stack'),
+    'deprecations' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+            'ignore_exceptions' => false,
+        ],
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+        'stderr' => [
+            'driver' => 'monolog',
+            'level' => env('LOG_LEVEL', 'debug'),
+            'handler' => StreamHandler::class,
+            'formatter' => env('LOG_STDERR_FORMATTER'),
+            'with' => [
+                'stream' => 'php://stderr',
+            ],
+        ],
+        'syslog' => [
+            'driver' => 'syslog',
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+        'errorlog' => [
+            'driver' => 'errorlog',
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+        'null' => [
+            'driver' => 'monolog',
+            'handler' => SyslogUdpHandler::class,
+        ],
+    ],
+];
